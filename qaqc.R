37175bd869face1454b0164945e89a985501e800
jacobmpeters <jcbptrs@gmail.com>
2023-04-17 16:31:14 -0400

    Added get_module1_dataget_merged_module_1_data() and ValidValues ranges

--- qaqc.R
+++ qaqc.R
@@ -8,6 +8,7 @@ library(gargle)
 library(readxl)
 library(stringr)
 library(glue)
+library(janitor)
 ####################### Un-comment to use plummer api ##########################
 # #* heartbeat...
 # #* @get /
@@ -25,17 +26,26 @@ library(glue)
 
 ################################################################################
 ####################    Define Parameters Here     #############################
-# For QC_REPORT, select "recruitment" or "biospecimen"
-QC_REPORT  <- "biospecimen"
-rules_file <- "qc_rules_biospecimen.xlsx"
-#sheet      <- "TestMichelles" 
-#sheet      <- "Kelseys" 
-sheet      <- "Jings"
-tier       <- "stg"
+################################################################################
+# For QC_REPORT, select "recruitment", "biospecimen" or "module1"
+
+### Biospecimen
+# QC_REPORT  <- "biospecimen"
+# rules_file <- "qc_rules_biospecimen.xlsx"
+# sheet      <- "TestMichelles"
+# tier       <- "prod"
+
+### Recruitment
 # QC_REPORT  <- "recruitment"
-# rules_file <- "qc_rules_recruitment_04_10_2023.xlsx"
-# sheet      <- NULL
-# tier       <- "stg"
+# rules_file <- "qc_rules_recruitment.xlsx"
+# sheet      <- NULL # "on_deck"
+# tier       <- "prod"
+
+### Module 1
+QC_REPORT  <- "module1"
+rules_file <- "qc_rules_module1.xlsx"
+sheet      <- NULL # "on_deck"
+tier       <- "prod"
 ################################################################################
 ################################################################################
 
@@ -270,7 +280,15 @@ valid <- function(data,ids,...){
   l=list(...) # Column names that are passed in ...
   
   ## select all the invalid rows. and save it in the "failed" tibble...
-  failed <- data %>% filter( !!sym(l$ConceptID) %!in% l$ValidValues)
+  # failed if ConceptID is in ValidValues
+  #       --> If valid values starts with "c(", evaluate it as r code
+  #           i.e., "c(1:100)" becomes c(1,2,3,4,5,...,100)
+  failed <- data %>%
+    filter(!!sym(l$ConceptID) %!in% ifelse(
+      startsWith(l$ValidValues, "c("),
+      eval(parse(text = l$ValidValues)),
+      l$ValidValues
+    ))
   
   l <- prepare_list_for_report(l)
   
@@ -284,6 +302,12 @@ valid <- function(data,ids,...){
 crossvalid <- crossvalidly(valid)
 na_or_valid <- na_ok(valid)
 na_or_crossvalid <- na_ok(crossvalid)
+
+# Custom R Expression
+custom <- function(data, ids, ...) {
+  l=list(...) # Column names that are passed in ...
+  failed <- data %>% filter(eval(parse(text=l$Qctest)))
+}
 
 ## Check that date is before other date
 validBeforeDate <- function(data,ids,...){
@@ -549,6 +573,111 @@ loadData <- function(project, tables, where_clause, download_in_chunks=TRUE) {
 
 }
 
+get_merged_module_1_data <- function(project) {
+  
+  sql_M1_1 <- bq_project_query(project, query=glue("SELECT * FROM `{project}.FlatConnect.module1_v1_JP`"))
+  sql_M1_2 <- bq_project_query(project, query=glue("SELECT * FROM `{project}.FlatConnect.module1_v2_JP`"))
+  
+  M1_V1 <- bq_table_download(sql_M1_1,bigint = "integer64") #1436 #1436 vars: 1507 01112023
+  M1_V2 <- bq_table_download(sql_M1_2,bigint = "integer64") #2333 #3033 01112023 var:1531
+  
+  # Check that it doesn't match any non-number
+  numbers_only <- function(x) !grepl("\\D", x)
+  mod1_v1 <- M1_V1
+  cnames <- names(M1_V1)
+  
+  ###to check variables and convert to numeric
+  for (i in 1: length(cnames)){
+    varname <- cnames[i]
+    var     <-pull(mod1_v1,varname)
+    mod1_v1[,cnames[i]] <- ifelse(numbers_only(var), as.numeric(as.character(var)), var)
+  }
+  mod1_v2 <- M1_V2
+  cnames  <- names(M1_V2)
+  
+  ###to check variables and convert to numeric
+  for (i in 1: length(cnames)){
+    varname <- cnames[i]
+    var     <- pull(mod1_v2,varname)
+    mod1_v2[,cnames[i]] <- ifelse(numbers_only(var), as.numeric(as.character(var)), var)
+  }
+  
+  M1_V1.var   <- colnames(M1_V1)
+  M1_V2.var   <- colnames(M1_V2)
+  var.matched <- M1_V1.var[which(M1_V1.var %in% M1_V2.var)]
+  length(var.matched)  #1275 #1278 vars 01112023
+  
+  V1_only_vars <- colnames(M1_V1)[colnames(M1_V1) %!in% var.matched] #232 #229 01112023
+  V2_only_vars <- colnames(M1_V2)[colnames(M1_V2) %!in% var.matched] #253 #253 01112023
+  
+  length(M1_V1$Connect_ID[M1_V1$Connect_ID %in% M1_V2$Connect_ID])
+  #[1] 62 with completing both versions of M1
+  
+  common.IDs <- M1_V1$Connect_ID[M1_V1$Connect_ID %in% M1_V2$Connect_ID]
+  M1_V1_common <- mod1_v1[,var.matched]
+  
+  M1_V2_common <- mod1_v2[,var.matched]
+  M1_V1_common$version <- 1
+  M1_V2_common$version <- 2
+  
+  
+  #to check the empty columns in both version common part
+  empty_columns_V1 <- colSums(is.na(M1_V1_common) | M1_V1_common == "") == nrow(M1_V1_common)
+  empty_columns_V2 <- colSums(is.na(M1_V2_common) | M1_V2_common == "") == nrow(M1_V2_common)
+  length(colnames(M1_V1_common[,empty_columns_V1]))
+  #[1] "COMPLETED"               "COMPLETED_TS"            "D_317093647_D_206625031" "D_317093647_D_261863326"
+  #[5] "D_406011084_D_197994844" "D_406011084_D_380275309" "D_593017220_D_106010694" "D_593017220_D_434556295"
+  
+  length(colnames(M1_V2_common[,empty_columns_V2]))
+  # [1] "COMPLETED"                                                                   
+  # [2] "COMPLETED_TS"                                                                
+  # [3] "D_317093647_D_206625031"                                                     
+  # [4] "D_317093647_D_261863326"                                                     
+  # [5] "D_384881609_1_1_D_206625031_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1"
+  # [6] "D_483975329_1_1_D_206625031_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1"
+  # [7] "D_527057404_D_206625031"                                                     
+  # [8] "D_750420077_D_505282171"                                                     
+  # [9] "D_750420077_D_578416151"                                                     
+  # [10] "D_750420077_D_846483618"
+  
+  
+  m1_v1_only <- mod1_v1[,c("Connect_ID", V1_only_vars)]
+  m1_v2_only <- mod1_v2[,c("Connect_ID", V2_only_vars)]
+  m1_v1_only$version <- 1
+  m1_v2_only$version <- 2
+  #for (i in 1:length)
+  
+  #library(janitor)
+  
+  m1_common <- rbind(M1_V1_common,M1_V2_common)
+  #m1_common_v1 <- merge(m1_common, m1_v1_only, by="Connect_ID",all.x=TRUE)
+  #m1_combined_v1v2 <- merge(m1_common_v1,m1_v2_only,by="Connect_ID",all.x=TRUE)
+  
+  verson_dup <- m1_common[which(m1_common$Connect_ID %in% common.IDs),]
+  
+  M1_combined <- rbind(M1_V1_common)
+  # M1_combined <- 
+  M1 <- merge(verson_dup,m1_v1_only,by=c("Connect_ID","version"))
+  
+  ###to combine two versions of M1 excluding the version 1 from the 62 with two version outputs requested by Kowlsey 0111/2023
+  m1_common_nodup <- rbind(M1_V1_common[which(M1_V1_common$Connect_ID %!in% common.IDs),],M1_V2_common)
+  m1_common_v1_nodup <- merge(m1_common_nodup, m1_v1_only[which(M1_V1_common$Connect_ID %!in% common.IDs),], by="Connect_ID",all.x=TRUE)
+  m1_combined_v1v2_nodup <- merge(m1_common_v1_nodup,m1_v2_only[,(colnames(m1_v2_only) %!in% "version") ],by="Connect_ID",all.x=TRUE)
+
+    ###### if you want to combine the participants table as well
+  parts <- glue("SELECT Connect_ID, token, d_512820379, d_471593703, state_d_934298480, d_230663853,
+d_335767902, d_982402227, d_919254129, d_699625233, d_564964481, d_795827569, d_544150384,
+d_371067537, d_430551721, d_821247024, d_914594314,  d_827220437,
+d_949302066 , d_517311251, d_205553981, d_117249500  FROM `{project}.FlatConnect.participants_JP`
+where Connect_ID IS NOT NULL")
+  parts_table <- bq_project_query(project, parts)
+  parts_data <- bq_table_download(parts_table, bigint = "integer64")
+
+  parts_data$Connect_ID <- as.numeric(parts_data$Connect_ID) ###need to convert type- m1... is double and parts is character
+
+  merged_data <- left_join(m1_combined_v1v2_nodup, parts_data, by="Connect_ID")
+  merged_data
+}
 
 get_explanation <- function(x) {
   x <- x %>%
@@ -700,13 +829,18 @@ if (loadFromBQ){
     tables              <- c('biospecimen_JP','participants_JP')
     where_clause        <- "WHERE Connect_ID IS NOT NULL"
     download_in_chunks  <- TRUE
+    data <- loadData(project, tables, where_clause, download_in_chunks=download_in_chunks)
   } 
   else if (QC_REPORT == "recruitment") {
     tables              <- c('participants_JP')
     where_clause        <- ""
     download_in_chunks  <- TRUE
+    data <- loadData(project, tables, where_clause, download_in_chunks=download_in_chunks)
   }
-  data <- loadData(project, tables, where_clause, download_in_chunks=download_in_chunks)
+  else if (QC_REPORT == "module1") {
+    data <- get_merged_module_1_data(project)
+  }
+
   
 }else{
   data_file <- "data.json"
@@ -779,3 +913,5 @@ if (push_to_cloud_storage) {
 }
 
 #}
+
+
